e275ccc6881de019f039589183dc59d41efd2e61
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-09-13 14:18:57 +0530

    testing DEBUGval and fontsize adjustments

--- gitpro/settings.py
+++ gitpro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not ("False"==str(os.environ.get('DEBUG')))
-# DEBUG = False
+print(DEBUG)
 ALLOWED_HOSTS = ['*']
 
 
