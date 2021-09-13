763456a913c2c4f4ed83650c3b4ca3af7d80c057
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-09-13 14:22:29 +0530

    finalised debug code

--- gitpro/settings.py
+++ gitpro/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not ("False"==str(os.environ.get('DEBUG')))
-print(DEBUG)
 ALLOWED_HOSTS = ['*']
 
 
